3ca5839785896e9aeb3e87f376d8912b08e6c0be
hugoescalda21 <88105920+hugoescalda21@users.noreply.github.com>
2026-09-21 18:22:11 -0300

    Add files via upload

--- ver/service-worker.js
+++ ver/service-worker.js
@@ -13,7 +13,7 @@ firebase.initializeApp({
 
 const messaging = firebase.messaging();
 
-const CACHE_NAME = 'ver-salon-v5';
+const CACHE_NAME = 'ver-salon-v6';
 const ASSETS = [
   './ver.html',
   './manifest-ver.json',
